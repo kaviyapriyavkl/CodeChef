b5fb35b90415183707cfc8a6ad3caa543df736cd
kavikannavkl <kavikannavkl@gmail.com>
2023-07-30 16:02:36 +0530

    First commit

---
+++ python/brokenstring.py
@@ -0,0 +1,11 @@
+# cook your dish here
+t= int(input())
+for i in range(t):
+    n=int(input())
+    s=input()
+    m=n//2
+    if(s[0:m]==s[m:n]):
+        print("YES")
+    else:
+        print("NO")
+    
