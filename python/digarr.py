114a9b5e8dc163f8fdb1f26c2f56a42fa08711f0
kavikannavkl <kavikannavkl@gmail.com>
2023-08-09 22:16:15 +0530

    First commit

---
+++ python/digarr.py
@@ -0,0 +1,10 @@
+# cook your dish here
+t=int(input())
+
+for i in range(t):
+    s=int(input())
+    a= str(input())
+    if('0' in a or '5' in a):
+        print("Yes")
+    else:
+        print("No")
