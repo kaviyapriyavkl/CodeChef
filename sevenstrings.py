f9d0a78b5a01b95e8070ca1a38fb57679e605fdd
Kaviyapriya V K <119722892+kaviyapriyavkl@users.noreply.github.com>
2023-08-17 23:23:54 +0530

    Delete sevenstrings.py

--- sevenstrings.py
+++
@@ -1,9 +0,0 @@
-# cook your dish here
-t=int(input())
-for i in range(t):
-    x,y=map(int,input().split())
-    z=x*y
-    if(len(str(z))==5 and z!=0):
-        print("YES")
-    else:
-        print("NO")
