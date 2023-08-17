a252ca4d874a86ffab95f567923b1196d93c0425
kavikannavkl <kavikannavkl@gmail.com>
2023-08-17 23:19:49 +0530

    first commit

---
+++ python/am15.py
@@ -0,0 +1,15 @@
+# cook your dish here
+t=int(input())
+a=list(map(int,input().split()))
+even=0
+odd=0
+for i in a:
+        if(i%2==0):
+            even+=1
+        else:
+            odd+=1
+    
+if(even>odd):
+        print("READY FOR BATTLE")
+else:
+        print("NOT READY")
