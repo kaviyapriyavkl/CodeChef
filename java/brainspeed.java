ceecf800d15b13294987b1ff6c766f46b5e14588
kavikannavkl <kavikannavkl@gmail.com>
2023-08-27 22:06:01 +0530

    First Commit

---
+++ java/brainspeed.java
@@ -0,0 +1,25 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class brainspeed
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc= new Scanner(System.in);
+		int x=sc.nextInt();
+		int y=sc.nextInt();
+		if(y<=x)
+		{
+		    System.out.println("NO");
+		}
+		else
+		{
+		    System.out.println("YES");
+		}
+	}
+}
