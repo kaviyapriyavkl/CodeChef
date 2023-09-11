9e41eb947a625de2162af1c8e4c2f51d071849b7
kavikannavkl <kavikannavkl@gmail.com>
2023-09-11 22:28:13 +0530

    First Commit

---
+++ python/pass.py
@@ -0,0 +1,8 @@
+t=int(input())
+for i in range(t):
+    a,b,c=map(int,input().split())
+    if(a>=10 and b>=10 and c>=10 and (a+b+c)>=100):
+        
+            print("PASS")
+    else:
+        print("FAIL")
