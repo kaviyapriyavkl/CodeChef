e4aeb8476ccd59d31f7da9d443791146a3bf2579
kavikannavkl <kavikannavkl@gmail.com>
2023-09-25 23:15:09 +0530

    first Commit

---
+++ python/amount.py
@@ -0,0 +1,8 @@
+# cook your dish here
+t=int(input())
+for i in range(t):
+    a,b,c,d=map(int,input().split())
+    if(a+b>=d or b+c>=d or a+c>=d or a>=d or b>=d or c>=d):
+        print("YES")
+    else:
+        print("NO")
