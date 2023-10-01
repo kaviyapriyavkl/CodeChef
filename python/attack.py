0d6d9ea997688f48131881d43d91f54a2beba68d
kavikannavkl <kavikannavkl@gmail.com>
2023-10-01 11:37:03 +0530

    first commit

---
+++ python/attack.py
@@ -0,0 +1,9 @@
+# cook your dish here
+t=int(input())
+for i in range(t):
+    h,x,y=map(int,input().split())
+    a=h-y
+    if(a%x==0):
+        print((a//x)+1)
+    else:
+        print((a//x)+2)
