0aa64ae7d2865f33c312ed619637ec22622050a8
kavikannavkl <kavikannavkl@gmail.com>
2023-10-03 14:15:38 +0530

    First commit

---
+++ python/aorb.py
@@ -0,0 +1,9 @@
+# cook your dish here
+t=int(input())
+for i in range(t):
+    a,b=map(int,input().split())
+    t1=(500-(a*2))+(1000-((b+a)*4))
+    t2=(1000-(b*4))+(500-((b+a)*2))
+    print(max(t1,t2))
+    # print(500-(a*2))
+    # print(1000-(a*4))
