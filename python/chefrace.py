09fb1791905c82228df31f288379602e93d17e64
kavikannavkl <kavikannavkl@gmail.com>
2023-10-04 15:07:53 +0530

    First Commit

---
+++ python/chefrace.py
@@ -0,0 +1,15 @@
+# cook your dish here
+t=int(input())
+for i in range(t):
+    a,b,c,d=map(int,input().split())
+    n=0
+    if((a==c or a==d) and (b==c or b==d)):
+        print(0)
+    elif((a==c or a==d) or (b==c or b==d)):
+        print(1)
+
+    else:
+        print(2)
+    
+    
+    
