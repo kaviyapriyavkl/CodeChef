430709365bc02c537f154f7e2621dd057cbd9b34
kavikannavkl <kavikannavkl@gmail.com>
2023-10-09 23:13:45 +0530

    First commit

---
+++ python/birdform.py
@@ -0,0 +1,12 @@
+# cook your dish here
+t=int(input())
+for i in range(t):
+    x,y,z=map(int,input().split())
+    if(z%x==0 and z%y==0):
+        print("any")
+    elif(z%x!=0 and z%y==0):
+        print("Duck")
+    elif(z%x==0 and z%y!=0):
+        print("CHICKEN")
+    else:
+        print("NONE")
