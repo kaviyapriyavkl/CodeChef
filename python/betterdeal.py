804f913cab3974f44c0affe81340f118d86d90f9
kavikannavkl <kavikannavkl@gmail.com>
2023-10-10 15:19:05 +0530

    FIRST COMMIT

---
+++ python/betterdeal.py
@@ -0,0 +1,13 @@
+# cook your dish here
+t=int(input())
+for i in range(t):
+    a,b=map(int,input().split())
+    a=100-(a*100)/100
+    b=200-(b*200)/100
+    if(a==b):
+        print("BOTH")
+    elif(a>b):
+        print("SECOND")
+    else:
+        print("FIRST")
+    
