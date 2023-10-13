e51bc787e3ae5313dc85aab219f88d6630d58e3e
kavikannavkl <kavikannavkl@gmail.com>
2023-10-13 09:59:11 +0530

    first commit

---
+++ python/atm2.py
@@ -0,0 +1,19 @@
+# cook your dish here
+t=int(input())
+for i in range(t):
+    c=0
+    tot,mon=map(int,input().split())
+    lst=list(map(int,input().split()))
+    for i in lst:
+        if(mon>=i):
+            print("1",end="")
+            mon-=i
+        else:
+            print("0",end="")
+        c+=1
+    if(c==len(lst)):
+        print("")
+        
+      
+        
+       
