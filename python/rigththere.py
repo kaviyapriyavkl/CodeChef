907595c3cb644c41e34209583be054758feb6867
kavikannavkl <kavikannavkl@gmail.com>
2023-10-21 19:21:19 +0530

    first commit

---
+++ python/rigththere.py
@@ -0,0 +1,9 @@
+# cook your dish here
+# cook your dish here
+T=int(input())
+for i in range(T):
+    N,X=map(int,input().split())
+    if(N<=X):
+        print("YES")
+    else:
+        print("NO")
